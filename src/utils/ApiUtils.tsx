3848368ed9d7d9c9554d2c4da4e63104613eef85
Alexey "akamych" Kalinin <akamych@gmail.com>
2024-02-18 21:10:05 +0100

    api requests utils

---
+++ src/utils/ApiUtils.tsx
@@ -0,0 +1,50 @@
+import { API_URL_NORMA } from "../constants/api";
+
+type ResponseType = {
+  success: boolean;
+  data: unknown;
+};
+
+export const checkResponse = (res: Response): ResponseType | Promise<any> => {
+  return res.ok ? res.json() : res.json().then((err) => Promise.reject(err));
+};
+
+export const refreshToken = () => {
+  return fetch(`${API_URL_NORMA}/auth/token`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json;charset=utf-8",
+    },
+    body: JSON.stringify({
+      token: localStorage.getItem("refreshToken"),
+    }),
+  })
+    .then(checkResponse)
+    .then((refreshData) => {
+      if (!refreshData.success) {
+        return Promise.reject(refreshData);
+      }
+      localStorage.setItem("refreshToken", refreshData.refreshToken);
+      localStorage.setItem("accessToken", refreshData.accessToken);
+      return refreshData;
+    });
+};
+
+export const fetchWithRefresh = async (
+  url: string,
+  options: Record<string, any>
+) => {
+  try {
+    const res = await fetch(url, options);
+    return await checkResponse(res);
+  } catch (err: any) {
+    if (err.message === "jwt expired") {
+      const refreshData = await refreshToken(); //обновляем токен
+      options.headers.authorization = refreshData.accessToken;
+      const res = await fetch(url, options); //повторяем запрос
+      return await checkResponse(res);
+    } else {
+      return Promise.reject(err);
+    }
+  }
+};
