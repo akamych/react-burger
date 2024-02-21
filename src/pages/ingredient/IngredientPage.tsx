d5c8ad6646f2632f2aa54742da81c933405c4794
Alexey "akamych" Kalinin <akamych@gmail.com>
2024-02-21 14:03:42 +0100

    forgot h1

--- src/pages/ingredient/IngredientPage.tsx
+++ src/pages/ingredient/IngredientPage.tsx
@@ -6,9 +6,12 @@ import { selectFetchedIngredients } from "../../services/reducers/IngredientsRed
 import { IngredientType } from "../../types/Ingredient.type";
 import { Nullable } from "../../types/common.type";
 import IngredientDetails from "../../components/modal/ingredients/IngredientDetails";
+import { useTranslation } from "react-i18next";
+import styles from "./IngredientPage.module.css";
 
 const IngredientPage = () => {
   let { ingredientId } = useParams();
+  const { t } = useTranslation("ingredients");
   const ingredients = useSelector(selectFetchedIngredients);
   const [ingredient, setIngredient] = useState<Nullable<IngredientType>>(null);
   const dispatch = useDispatch<AppDispatch>();
@@ -20,7 +23,10 @@ const IngredientPage = () => {
   }, [ingredients, ingredientId, dispatch]);
 
   return (
-    <main>
+    <main className={styles.main}>
+      <h1 className="text text_type_main-large mt-10 mb-5">
+        {t("h3.details")}
+      </h1>
       {ingredient !== null && <IngredientDetails ingredient={ingredient} />}
     </main>
   );
